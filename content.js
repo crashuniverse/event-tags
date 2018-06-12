6fc5bf1223d8262cbdbabd79e2145936a381ba72
Priya Ranjan Singh <mail@priyaranjan.net>
2018-06-12 17:58:14 +0200

    add artist listener count & tags from lastfm

--- content.js
+++ content.js
@@ -3,5 +3,19 @@ console.log('content scripts ready to go');
 const artistNodes = document.querySelectorAll('.artists.summary strong');
 artistNodes.forEach((artistNode, index) => {
   const text = artistNode.innerText;
-  console.log(index, text);
+  const artist = encodeURIComponent(text);
+  const url = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=1d135f71576245c162267ffee970359e&format=json`;
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      const listeners = data.artist && data.artist.stats && data.artist.stats.listeners;
+      const tags = data.artist && data.artist.tags && data.artist.tags.tag.length &&
+        data.artist.tags.tag.map(tag => tag.name);
+      const listenersNode = document.createElement('div');
+      listenersNode.innerText = listeners || 0;
+      artistNode.parentElement.parentElement.parentElement.append(listenersNode);
+      const tagsNode = document.createElement('div');
+      tagsNode.innerText = tags && tags.length ? tags.join(', ') : '';
+      artistNode.parentElement.parentElement.parentElement.append(tagsNode);
+    })
 });
